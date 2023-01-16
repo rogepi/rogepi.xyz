8576f792cfdb682f4f56b8c03256d3f88636cb87
rogepi <zzr1119@outlook.com>
2023-01-17 00:54:13 +0800

    ✨ feat: add analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,32 @@
+import Hero from '../public/images/pmb.png'
+
+import Image from 'next/image'
+
 export default function Home() {
-  return <main>Hello World</main>
+  return (
+    <>
+      <section className="container max-w-[800px] mt-10">
+        <Image src={Hero} width={150} alt="Hero image" priority />
+        <h1 className="my-5 text-3xl font-bold sm:text-4xl lg:text-5xl">
+          你好呀 我是阿荣！
+        </h1>
+        <p className="text-slate-700 dark:text-slate-300 text-sm sm:text-base">
+          一个前端开发者、咖啡爱好者、宝可梦训练家，喜欢探究新的开源项目
+        </p>
+        <div className="my-5 flex gap-5">
+          <button className="btn-p" type="button">
+            Github
+          </button>
+          <button className="btn" type="button">
+            掘金
+          </button>
+        </div>
+      </section>
+      <section className="container max-w-[800px] mt-10">
+        <h1 className="my-5 text-2xl font-bold sm:text-3xl lg:text-4xl">
+          最新文章
+        </h1>
+      </section>
+    </>
+  )
 }
--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { VercelAnalytics } from './analytics'
 import Footer from './footer'
 import Header from './header'
 
@@ -6,8 +7,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen bg-gray-50 dark:bg-neutral-900">
       <Header />
-      <main className="flex-grow w-full max-w-2xl mx-auto">{children}</main>
+      <main className="flex-1">{children}</main>
       <Footer />
+      <VercelAnalytics />
     </div>
   )
 }
